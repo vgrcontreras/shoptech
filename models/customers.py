72c80214df9614157814325fc40819cabc05663c
Victor Contreras <contreras3991@gmail.com>
2025-01-19 19:43:45 -0300

    :sparkles: feat(models): create customer sqlalchemy model

---
+++ models/customers.py
@@ -0,0 +1,24 @@
+from datetime import date, datetime
+
+from sqlalchemy.orm import Mapped, mapped_column
+
+from core.database import table_registry
+
+
+@table_registry.mapped_as_dataclass
+class Customer:
+    __tablename__ = 'customers'
+
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    first_name: Mapped[str] = mapped_column(nullable=False)
+    last_name: Mapped[str] = mapped_column(nullable=False)
+    email: Mapped[str] = mapped_column(nullable=False)
+    phone_number: Mapped[str] = mapped_column(nullable=False)
+    gender: Mapped[str] = mapped_column(nullable=False)
+    date_of_birth: Mapped[date] = mapped_column(nullable=False)
+    address: Mapped[str] = mapped_column(nullable=False)
+    city: Mapped[str] = mapped_column(nullable=False)
+    state: Mapped[str] = mapped_column(nullable=False)
+    country: Mapped[str] = mapped_column(nullable=False)
+    postal_code: Mapped[str] = mapped_column(nullable=False)
+    created_at: Mapped[datetime] = mapped_column(nullable=False)
