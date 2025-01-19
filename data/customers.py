ce0bd7e1ef434b1b6a06eebc404d21c457f7e15b
Victor Contreras <contreras3991@gmail.com>
2025-01-19 19:44:34 -0300

    :sparkles: feat: create customers fake data generator

---
+++ data/customers.py
@@ -0,0 +1,24 @@
+from faker import Faker
+
+from models.customers import Customer
+
+fake = Faker(locale='pt-BR')
+
+
+def generate_customer_data() -> Customer:
+    customers = Customer(
+        first_name=fake.first_name(),
+        last_name=fake.last_name(),
+        email=fake.safe_email(),
+        phone_number=fake.cellphone_number(),
+        gender=fake.random_element(['Masculino', 'Feminino', 'Outros']),
+        date_of_birth=fake.date_of_birth(minimum_age=18, maximum_age=50),
+        address=fake.address().replace('\n', ', '),
+        city=fake.city(),
+        state=fake.bairro(),
+        country=fake.country(),
+        postal_code=fake.postcode(),
+        created_at=fake.date_time_this_decade(),
+    )
+
+    return customers
