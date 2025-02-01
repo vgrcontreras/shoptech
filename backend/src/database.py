a63fa4cb714a8d555b2fb56ac7fc46fdb9d72247
Victor Contreras <contreras3991@gmail.com>
2025-01-31 23:03:38 -0300

    add context manager for database session

--- backend/src/database.py
+++ backend/src/database.py
@@ -1,8 +1,16 @@
+from contextlib import contextmanager
+
 from sqlalchemy import create_engine
-from sqlalchemy.orm import registry
+from sqlalchemy.orm import Session, registry
 
 from src.settings import settings
 
 table_registry = registry()
 
 engine = create_engine(settings.DATABASE_URL)
+
+
+@contextmanager
+def get_session():  # pragma: no cover
+    with Session(engine) as session:
+        yield session
